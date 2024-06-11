1f1b3c3d5294e6448c685d17596537d6c0f4596b
Québec <95536686+hsu26zq@users.noreply.github.com>
2024-06-11 23:30:45 +0800

    Create my_transaction.sv

---
+++ src/RTL/UVM/my_transaction.sv
@@ -0,0 +1,13 @@
+import uvm_pkg::*;
+
+class my_transaction extends uvm_sequence_item;
+  /*
+  rand real real_A, real_B, real_Z;
+  logic ENABLE, READY;
+  rand logic [63:0] A, B, Z;
+  
+  `uvm_object_utils(my_transaction)
+  function new(string name = "my_transaction");
+    super.new(name);
+  endfunction */
+endclass
